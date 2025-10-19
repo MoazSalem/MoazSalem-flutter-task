441ef96d5f33e6dd80957d86efc9e8e9c257f467
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-10-19 13:40:10 +0300

    Change status bar and navigation bar color to transparent in older android versions

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:otex/core/navigation/app_router.dart';
 import 'package:otex/core/theme/app_theme.dart';
 import 'package:otex/l10n/app_localizations.dart';
@@ -8,15 +9,24 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      // App fully supports both arabic and english languages, but i didn't implement a switch language feature
-      locale: const Locale('ar'),
-      title: 'Otex',
-      theme: AppTheme.lightTheme,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: const [Locale('en'), Locale('ar')],
-      routerConfig: AppRouter().router,
+    // change status bar and navigation bar color to transparent in older android versions
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent,
+        systemNavigationBarIconBrightness: Brightness.dark,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        // App fully supports both arabic and english languages, but i didn't implement a switch language feature
+        locale: const Locale('ar'),
+        title: 'Otex',
+        theme: AppTheme.lightTheme,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: const [Locale('en'), Locale('ar')],
+        routerConfig: AppRouter().router,
+      ),
     );
   }
 }
